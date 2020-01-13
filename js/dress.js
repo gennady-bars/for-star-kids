e91fc350ea93fbb4e32297ff7e3c06b0d15a6ec3
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-01-13 13:19:41 +0300

    add dress.html and dress.js

---
+++ js/dress.js
@@ -0,0 +1,33 @@
+let count = 0;
+let limit = 16;
+
+loadMore();
+
+function loadMore() {	
+	let block = Array(12).fill(0).map(x => count < limit? `<div class="col-md-4 col-lg-3 col-sm-6">
+	<div class="card">
+	  <img src="img/dress${++count}.jpg"  alt="..." />
+	  <div class="card-body">
+		<h3 class="post-category">Какое-то название</h3>
+		<h5 class="post-title"> еще что-то</h5>
+		<p class="card-text">
+		  описание чего-то
+		</p>
+	  </div>
+	</div>
+  </div>`:'');
+  
+  $('#load-more').append(block);
+
+}
+
+  $(window).scroll(function(){
+	let targetBlock = $('.after-posts');
+	let targetPos = targetBlock.offset().top;
+	let winHeight = $(window).height();
+	let scrollToElem = targetPos - winHeight;
+  	let winScrollTop = $(this).scrollTop();
+  	if(winScrollTop > scrollToElem && count < limit){
+		loadMore();
+  	}
+  });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(function(){
-	$('.load-more').on('scroll', loadMore);
-});
+// $(function(){
+// 	$('.load-more').on('scroll', loadMore);
+// });
 
 let count = 0;
 let limit = 12;
@@ -28,21 +28,21 @@ function loadMore() {
 	// 		btn.attr('disabled', false);
 	// 	}
 	// });
-	let block = Array(12).fill(0).map(x => count < limit? `<div class="col-md-4 col-lg-3 col-sm-6">
-	<div class="card">
-	  <img src="img/dress${++count}.jpg"  alt="..." />
-	  <div class="card-body">
-		<h3 class="post-category">Какое-то название</h3>
-		<h5 class="post-title"> еще что-то</h5>
-		<p class="card-text">
-		  описание чего-то
+	let block = Array(12).fill(0).map(x => count < limit? 
+		`<div class="col-md-4 col-lg-3 col-sm-6">
+			<div class="card">
+			<img src="img/dress${++count}.jpg"  alt="..." />
+			<div class="card-body">
+				<h3 class="post-category">Какое-то название</h3>
+				<h5 class="post-title"> еще что-то</h5>
+				<p class="card-text">
+				описание чего-то
 
-		</p>
-	  </div>
-	</div>
-  </div>`:'');
-  
-  $('#load-more').append(block);
+				</p>
+			</div>
+			</div>
+		</div>`:'');
+  	$('#load-more').append(block);
 
 }
 
