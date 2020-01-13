ad802849cfa8da01851187dd4f955e05ff7a02bb
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-01-13 09:33:03 +0300

    add loadMore on scroll

--- js/main.js
+++ js/main.js
@@ -1,28 +1,105 @@
 $(function(){
-
-	$('.load-more').on('click', function(){
-		const btn = $(this);
-		const loader = btn.find('span');
-		$.ajax({
-			url: '/data.html',
-			type: 'GET',
-			beforeSend: function(){
-				btn.attr('disabled', true);
-				loader.addClass('d-inline-block');
-			},
-			success: function(responce){
-				setTimeout(function(){
-					loader.removeClass('d-inline-block');
-					btn.attr('disabled', false);
-					$('.after-posts').before(responce);
-				}, 1000);
-			},
-			error: function(){
-				alert('Error!');
-				loader.removeClass('d-inline-block');
-				btn.attr('disabled', false);
-			}
-		});
-	});
-
+	$('.load-more').on('scroll', loadMore);
 });
+
+let count = 4;
+let limit = 10;
+
+function loadMore() {
+	// const btn = $(this);
+	// const loader = btn.find('span');
+	// $.ajax({
+	// 	url: '/data.html',
+	// 	type: 'GET',
+	// 	beforeSend: function(){
+	// 		btn.attr('disabled', true);
+	// 		loader.addClass('d-inline-block');
+	// 	},
+	// 	success: function(responce){
+	// 		setTimeout(function(){
+	// 			// loader.removeClass('d-inline-block');
+	// 			btn.attr('disabled', false);
+	// 			$('.after-posts').before(responce);
+	// 		}, 1000);
+	// 	},
+	// 	error: function(){
+	// 		alert('Error!');
+	// 		loader.removeClass('d-inline-block');
+	// 		btn.attr('disabled', false);
+	// 	}
+	// });
+	let block = $(`<section class="grid-posts">
+	<div class="container">
+	  <div class="row">
+		<div class="col-md-6">
+		  <div class="card">
+			<img src="img/dress${++count}.jpg"  alt="..." />
+			<div class="card-body">
+			  <h3 class="post-category">Какое-то название</h3>
+			  <h5 class="post-title"> еще что-то</h5>
+			  <p class="card-text">
+				описание чего-то
+  
+			  </p>
+			</div>
+		  </div>
+		</div>
+		<div class="col-md-6">
+		  <div class="card">
+			<img src="img/dress${++count}.jpg"  alt="..." />
+			<div class="card-body">
+			  <h3 class="post-category">Какое-то название</h3>
+			  <h5 class="post-title"> еще что-то</h5>
+			  <p class="card-text">
+				описание чего-то
+  
+			  </p>
+			</div>
+		  </div>
+		</div>
+  
+		<div class="col-md-6">
+		  <div class="card">
+			<img src="img/dress${++count}.jpg"  alt="..." />
+			<div class="card-body">
+			  <h3 class="post-category">Какое-то название</h3>
+			  <h5 class="post-title"> еще что-то</h5>
+			  <p class="card-text">
+				описание чего-то
+  
+			  </p>
+			</div>
+		  </div>
+		</div>
+		<div class="col-md-6">
+		  <div class="card">
+			<img src="img/dress${++count}.jpg"  alt="..." />
+			<div class="card-body">
+			  <h3 class="post-category">Какое-то название</h3>
+			  <h5 class="post-title"> еще что-то</h5>
+			  <p class="card-text">
+				описание чего-то
+  
+			  </p>
+			</div>
+		  </div>
+		</div>
+		
+	  </div>
+	</div>
+  </section>`);
+  
+  $('#load-more').append(block);
+
+}
+
+  $(window).scroll(function(){
+	let targetBlock = $('.after-posts');
+	let targetPos = targetBlock.offset().top;
+	let winHeight = $(window).height();
+	let scrollToElem = targetPos - winHeight;
+  	let winScrollTop = $(this).scrollTop();
+  	if(winScrollTop > scrollToElem && count < limit){
+		loadMore();
+  	}
+  });
